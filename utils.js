0362ac7291bb90cea2267cb68bab6eef1e39ac75
蘇青陽 <138528756+tzd0011@users.noreply.github.com>
2026-03-07 04:13:56 +0800

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:12:48.742Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T20:13:55.067Z
+Commit: Add multi-language support
