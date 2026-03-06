30486404700a7b7956b8596754a1af15eb25330c
蘇青陽 <138528756+tzd0011@users.noreply.github.com>
2026-03-07 04:13:03 +0800

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:12:10.663Z
-Commit: Add dark mode support
+Timestamp: 2026-03-06T20:13:02.189Z
+Commit: Resolve null pointer exception in user service
