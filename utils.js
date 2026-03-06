14930db5937be5b49aef33cfd37bae054e5193e4
蘇青陽 <138528756+tzd0011@users.noreply.github.com>
2026-03-07 04:16:41 +0800

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:16:33.682Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T20:16:39.760Z
+Commit: Patch security vulnerability in authentication
