6f6bbcf9f274bbc1a84d84614bb405625f58736e
蘇青陽 <138528756+tzd0011@users.noreply.github.com>
2026-03-07 04:14:31 +0800

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:13:56.244Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T20:14:29.998Z
+Commit: Fix CSS styling bug on mobile devices
