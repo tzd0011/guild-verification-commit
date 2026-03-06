aee162979ec82e7390d73577c33b9ba1fc4fe900
蘇青陽 <138528756+tzd0011@users.noreply.github.com>
2026-03-07 04:13:45 +0800

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:13:42.852Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T20:13:44.022Z
+Commit: Fix memory leak in event handlers
